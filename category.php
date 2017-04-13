5de6350422efe15f4a5c09c17e66155557166986
xjnny <jennylcrowther@gmail.com>
2017-04-13 13:32:14 +1200

    cat template upgrade

--- category.php
+++ category.php
@@ -3,41 +3,35 @@
 * A Simple Category Template
 */
 
-get_header(); ?> 
+get_header(); ?>
 
-<section id="primary" class="site-content">
-<div id="content" role="main">
+<section class="galleries">
+  <div class="container">
 
-<?php 
-// Check if there are any posts to display
-if ( have_posts() ) : ?>
-
-<header class="archive-header">
-<h1 class="archive-title">Category: <?php single_cat_title( '', false ); ?></h1>
-
-
-<?php
-// Display optional category description
- if ( category_description() ) : ?>
-<div class="archive-meta"><?php echo category_description(); ?></div>
-<?php endif; ?>
-</header>
-
-<?php
-
-// The Loop
+<?php if ( have_posts() ) : 
 while ( have_posts() ) : the_post(); ?>
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
 
-<div class="entry">
-<?php the_content(); ?>
+  
+  
 
- <p class="postmetadata"><?php
-  comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
-?></p>
-</div>
+      
+       
 
+
+            <div class="image-container">
+              <a href="<?php the_permalink(); ?>">
+                <?php the_post_thumbnail('thumbnail', array('class' => 'image')); ?>
+                <div class="overlay">
+                  <div class="text"><?php the_title(); ?></div>
+                </div>
+              </a>
+            </div>
+            
+         
+      
+
+    
+  
 <?php endwhile; 
 
 else: ?>
@@ -48,6 +42,7 @@ else: ?>
 </div>
 </section>
 
+<?php
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+get_sidebar(); 
+get_footer(); 
